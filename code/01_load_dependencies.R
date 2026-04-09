bd1e14a641c3a0355db4cc65ae222e4fd2d4ab52
Danylo Orlov <dorlov.mph@gmail.com>
2026-04-09 12:27:46 -0400

    updating code to use python councilcount for 2023 ACS data

--- code/01_load_dependencies.R
+++ code/01_load_dependencies.R
@@ -3,8 +3,8 @@
 list.of.packages <- c("tidyverse", "janitor", "lubridate", "sf", "ggplot2", 
                       "leaflet", "leaflet.extras", "htmlwidgets", "RSocrata", 
                       "vroom", "mapview", "classInt", "openxlsx", "mapboxapi",
-                      "skimr", "DBI", "geoclient", "censusapi", "readxl", "htmltools", 
-                      "ggiraph", "gt", "gtExtras", "remotes", "showtext", "ggiraph"
+                      "skimr", "DBI", "censusapi", "readxl", "htmltools", 
+                      "ggiraph", "gt", "gtExtras", "showtext", "reticulate", "dotenv"
 )
 
 # checks if packages has been previously installed
@@ -16,12 +16,43 @@ if(length(new.packages)) install.packages(new.packages)
 # load standard packages
 lapply(list.of.packages, require, character.only = TRUE)
 
-# install and load custom councilcount package if not present
+
+# setup Python councilcount via reticulate 
+env_name <- "councilcount_env"
+
+# if the virtual environment doesn't exist, create it and install dependencies
+if (!virtualenv_exists(env_name)) {
+  message("Creating Python virtual environment...")
+  
+  # try to create the environment; if Python 3.9 is missing, download it first
+  tryCatch({
+    virtualenv_create(env_name, version = "3.9")
+  }, error = function(e) {
+    message("Suitable Python not found. Downloading Python 3.9 (this may take a minute)...")
+    reticulate::install_python(version = "3.9")
+    virtualenv_create(env_name, version = "3.9")
+  })
+  
+  message("Installing Python packages...")
+  virtualenv_install(env_name, "pandas")
+  virtualenv_install(env_name, "geopandas")
+  virtualenv_install(env_name, "dotenv")
+  virtualenv_install(env_name, "shapely")
+  virtualenv_install(env_name, "councilcount")
+}
+
+# force R to use this specific environment
+use_virtualenv(env_name, required = TRUE)
+
+# import the package
+cc <- import("councilcount")
+
+
+# setup R councilcount
 if (!require(councilcount)) {
   remotes::install_github("newyorkcitycouncil/councilcount")
   library(councilcount)
 }
-
 
 ## FUNCTIONS -----------------------------------------
 
